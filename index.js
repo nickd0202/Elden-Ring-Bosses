621a4e876049b43a870a982b12262e74f841c8b0
Nickita Danilovich <nickitadanilovich@gmail.com>
2023-01-20 15:54:20 -0500

    created profiles, small css

--- index.js
+++ index.js
@@ -12,14 +12,27 @@ function renderMonsters(monsters) {
     monsters.data.forEach((monster) =>  {
         const p = document.createElement('p')
         const div = document.createElement('div')
+        const contain = document.createElement('div')
+        const img = document.createElement('img')
         p.textContent = monster.name
         console.log(p)
+        img.src = monster.image
+        img.alt = "Avatar"
+        
+        img.className = "profile"
         p.className = "name"
         div.className = "card"
-        div.append(p)
+        contain.className = "container"
+        
+        contain.append(p)
+        div.append(img)
+        div.append(contain)
         allMonsters.append(div) 
     })
     //removes duplicate
-    document.querySelector("#all > div:nth-child(3) > p").remove()
+    document.querySelector("#all > div:nth-child(3)").remove()
+    document.querySelector("#all > div:nth-child(10)").remove()
+    document.querySelector("#all > div:nth-child(1) > img").src = "https://eldenring.wiki.fextralife.com/file/Elden-Ring/lesser_abductor_virgin_enemies_elden_ring_wiki_600px.jpg"
+
 }
 
