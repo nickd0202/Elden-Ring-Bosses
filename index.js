20c69cebd745fa4bfc03639effa518f2f8c70cbf
Dakota Vikdal <dakota.james19@hotmail.com>
2023-01-23 15:06:34 -0500

    css images

--- index.js
+++ index.js
@@ -38,7 +38,10 @@ function renderMonsters(monsters) {
     document.querySelector("#all > div:nth-child(3)").remove()
     document.querySelector("#all > div:nth-child(10)").remove()
     document.querySelector("#all > div:nth-child(1) > img").src = "https://eldenring.wiki.fextralife.com/file/Elden-Ring/lesser_abductor_virgin_enemies_elden_ring_wiki_600px.jpg"
-
+    
+    //If we want to change out the images that are fuzzy
+    //document.querySelector("#all > div:nth-child(2)").remove();
+    //document.querySelector("#all > div:nth-child(2) > img").src = "https://www.gamerguides.com/assets/media/14/451/alecto_black_knife_ringleader.png"
 
     
 }
